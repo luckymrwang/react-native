0ca0651e92577477426da4567e4b8ada4a9b8c14
luckymrwang <luckymrwang@163.com>
2016-05-02 17:08:12 +0800

    展现电影，添加样式

--- AwesomeProject/index.ios.js
+++ AwesomeProject/index.ios.js
@@ -7,25 +7,30 @@
 import React, {
   AppRegistry,
   Component,
+  Image,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
+var MOCKED_MOVIES_DATA = [
+  {title: '标题', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
+
 class AwesomeProject extends Component {
   render() {
+    var movie = MOCKED_MOVIES_DATA[0];
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Image source={{uri: movie.posters.thumbnail}} style={styles.thumbnail}/>
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>
+            {movie.title}
+          </Text>
+          <Text style={styles.year}>
+            {movie.year}
+          </Text>
+        </View>
       </View>
     );
   }
@@ -34,19 +39,25 @@ class AwesomeProject extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  thumbnail: {
+    width: 53,
+    height: 81,
   },
-  instructions: {
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  },
+  year: {
+    textAlign: 'center',
   },
 });
 
